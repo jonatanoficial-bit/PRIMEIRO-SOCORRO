f9cf0b193fa0b52ae378ace7b18b21c801d640db
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-04 19:55:25 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = "aps-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/app.js",
+  "./js/router.js",
+  "./js/ui.js",
+  "./js/store.js",
+  "./js/engine.js",
+  "./js/packs.js",
+  "./js/pages/home.js",
+  "./js/pages/study.js",
+  "./js/pages/simulation.js",
+  "./js/pages/packsPage.js",
+  "./js/pages/notfound.js",
+  "./content-packs/pack-xabcde-sbv-v1/manifest.json",
+  "./content-packs/pack-xabcde-sbv-v1/protocols.json",
+  "./content-packs/pack-xabcde-sbv-v1/scenarios.json",
+  "./content-packs/pack-xabcde-sbv-v1/references.json",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});

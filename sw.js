adf782e972dba7f1cec966ce79a948b06ea8a86e
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-04 21:23:08 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-const CACHE = "aps-cache-v4";
+const CACHE = "aps-cache-v42";
+
 const ASSETS = [
   "./",
-  "./index.html",
-  "./css/styles.css",
-  "./js/app.js",
-  "./js/router.js",
-  "./js/ui.js",
-  "./js/store.js",
-  "./js/engine.js",
-  "./js/packs.js",
-  "./js/registry.js",
-  "./js/signature.js",
-  "./js/pages/home.js",
-  "./js/pages/career.js",
-  "./js/pages/study.js",
-  "./js/pages/simulation.js",
-  "./js/pages/packsPage.js",
-  "./js/pages/notfound.js",
-  "./content-packs/pack-xabcde-sbv-v1/manifest.json",
-  "./content-packs/pack-xabcde-sbv-v1/protocols.json",
-  "./content-packs/pack-xabcde-sbv-v1/scenarios.json",
-  "./content-packs/pack-xabcde-sbv-v1/references.json",
-  "./manifest.webmanifest"
+  "./index.html?v=42",
+  "./css/styles.css?v=42",
+  "./js/app.js?v=42",
+  "./js/router.js?v=42",
+  "./js/ui.js?v=42",
+  "./js/store.js?v=42",
+  "./js/engine.js?v=42",
+  "./js/packs.js?v=42",
+  "./js/registry.js?v=42",
+  "./js/signature.js?v=42",
+  "./js/pages/home.js?v=42",
+  "./js/pages/career.js?v=42",
+  "./js/pages/study.js?v=42",
+  "./js/pages/simulation.js?v=42",
+  "./js/pages/packsPage.js?v=42",
+  "./js/pages/notfound.js?v=42",
+  "./content-packs/pack-xabcde-sbv-v1/manifest.json?v=42",
+  "./content-packs/pack-xabcde-sbv-v1/protocols.json?v=42",
+  "./content-packs/pack-xabcde-sbv-v1/scenarios.json?v=42",
+  "./content-packs/pack-xabcde-sbv-v1/references.json?v=42",
+  "./manifest.webmanifest?v=42"
 ];
 
 self.addEventListener("install", (e) => {
@@ -37,5 +38,7 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
 });
